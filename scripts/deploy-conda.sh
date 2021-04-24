a766f6adb8e64c58f2d9c5dfd362a056d95a4cbd
githubfoam <lequestionis@gmail.com>
2021-04-24 14:33:29 +0300

    м`орде

--- scripts/deploy-conda.sh
+++ scripts/deploy-conda.sh
@@ -8,4 +8,20 @@ set -o xtrace
 echo "========================================================================================="
 apt-get -qq update
 
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+hash -r
+
+conda config --set always_yes yes --set changeps1 no
+conda update -q conda
+conda info -a
+
+conda env create -n ~venv-basic-anomaly-detection python=$TRAVIS_PYTHON_VERSION  -f conda_environment.yml
+source activate ~venv-basic-anomaly-detection
+
+py.test
+
+deactivate
 echo "========================================================================================="
